f6140dadb77e6dfd0ada249d3896268abde0595f
R. Kyle Norris <r.kyle.norris@gmail.com>
2025-11-26 12:59:24 -0500

    fixed connection string, added test for connection in if __name__ block

--- db_target/session.py
+++ db_target/session.py
@@ -1,21 +1,22 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker, Session
 from dotenv import load_dotenv
 import os
 
 # Load environment variables from .env file
 load_dotenv()
-db_url_prefix = os.getenv("DB_URL_PREFIX")
-db_username = os.getenv("DB_USERNAME")
-db_password = os.getenv("DB_PASSWORD")
-db_server = os.getenv("DB_SERVER")
-db_name = os.getenv("DB_NAME")
+db_url_prefix = os.getenv("DOX_DB_URL_PREFIX")
+db_username = os.getenv("DOX_DB_USERNAME")
+db_password = os.getenv("DOX_DB_PASSWORD")
+db_server = os.getenv("DOX_DB_SERVER")
+db_name = os.getenv("DOX_DB_NAME")
+db_driver = os.getenv("DOX_DB_DRIVER")
 
 # create connection string
 CONNECTION_STRING = (
     f"{db_url_prefix}{db_username}:{db_password}@{db_server}/{db_name}"
-    "?driver=ODBC+Driver+17+for+SQL+Server"
-    "&TrustServerCertificate=yes"
+    f"?driver={db_driver}"
+    "&Encrypt=no&TrustServerCertificate=yes"
 )
 
 # Create the SQLAlchemy engine and session factory
@@ -30,3 +31,9 @@ def get_db_session() -> Session:
         Session: A new SQLAlchemy session.
     """
     return SessionLocal()
+
+
+if __name__ == "__main__":
+    with engine.connect() as connection:
+        print("Database connection successful.")
+        print(connection.execute(text("SELECT DB_NAME()")).scalar_one())
