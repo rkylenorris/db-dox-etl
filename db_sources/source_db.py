c2ff860959007aa760d2bd4604af1657b28e50b9
R. Kyle Norris <r.kyle.norris@gmail.com>
2025-12-01 23:20:39 -0500

    removed old invoke_query and added stream_query method for more effeciency, and removed the meta_data queries property as it isn't needed

--- db_sources/source_db.py
+++ db_sources/source_db.py
@@ -1,20 +1,18 @@
-import pandas as pd
 from sqlalchemy import create_engine, text
 from sqlalchemy.engine import Engine
 from typing import Any, Mapping
 
 from app_settings import AppSettings, get_app_settings
-from .query_model import Queries
+from .query_model import Query
 
 
 class SourceDatabase:
 
-    def __init__(self, db_name: str, settings: AppSettings = get_app_settings(), metadata_queries: Queries = Queries()):
+    def __init__(self, db_name: str, settings: AppSettings = get_app_settings()):
         self.name = db_name
         self.settings = settings
         self.db_config = settings.get_source_db_by_name(db_name)
         self.engine: Engine = self._create_engine()
-        self._queries: Queries = metadata_queries
 
     def _create_engine(self) -> Engine:
         if self.db_config is None:
@@ -23,20 +21,16 @@ class SourceDatabase:
 
         return create_engine(self.db_config.connection_string)
 
-    def invoke_query(self, sql: str, params: Mapping[str, Any] | None = None) -> pd.DataFrame | None:
+    def stream_query(self, sql_query: Query, params: Mapping[str, Any] | None = None, chunk_size: int = 5_000):
 
-        results: pd.DataFrame | None = None
-
-        if params is None:
-            results = pd.read_sql_query(
-                sql=text(sql),
-                con=self.engine
-            )
-        else:
-            results = pd.read_sql_query(
-                sql=text(sql),
-                con=self.engine,
-                params=params
+        with self.engine.connect() as conn:
+            cursor = conn.execution_options(stream_results=True).exec_driver_sql(
+                sql_query.sql_text,
+                params or {}
             )
 
-        return results
+            while True:
+                rows = cursor.fetchmany(chunk_size)
+                if not rows:
+                    break
+                yield rows
